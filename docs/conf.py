c8f319117f8d546f36e7fda9466882cfecff9d08
Samuel Carter <samuelcarter623@gmail.com>
2024-10-02 16:46:24 +0100

    remove default buttons

--- docs/conf.py
+++ docs/conf.py
@@ -24,28 +24,28 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
-# html_static_path = ['_static']
-# html_theme_options = {
-#     'navbar_title': "Paper Journal Club",
-#     'globaltoc_depth': 2,
-#     'globaltoc_collapse': True, 
-#     'globaltoc_includehidden': True,
-# }
-
-html_theme = 'furo'
-
-# Optional: Customize theme options
+html_static_path = ['_static']
 html_theme_options = {
-    "sidebar_hide_name": True,
-    "light_css_variables": {
-        "color-brand-primary": "#0072B2",
-        "color-brand-content": "#333333",
-    },
-    "default_color_mode": "dark",
-    "source_repository": None,
+    'navbar_title': "Paper Journal Club",
+    'globaltoc_depth': 2,
+    'globaltoc_collapse': True, 
+    'globaltoc_includehidden': True,
 }
+
+# html_theme = 'furo'
+
+# # Optional: Customize theme options
+# html_theme_options = {
+#     "sidebar_hide_name": True,
+#     "light_css_variables": {
+#         "color-brand-primary": "#0072B2",
+#         "color-brand-content": "#333333",
+#     },
+#     "default_color_mode": "dark",
+#     "source_repository": None,
+# }
 
 html_context = {
     "display_github": False,
