63c2acd2b3158a11fab85619d15c68ed3f6eb9ff
Samuel Carter <samuelcarter623@gmail.com>
2024-10-02 16:09:29 +0100

    sphinx-rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -24,11 +24,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'pydata_sphinx_theme'
-# html_static_path = ['_static']
-# html_theme_options = {
-#     'navbar_title': "Paper Journal Club",
-#     'globaltoc_depth': 2,
-#     'globaltoc_collapse': True, 
-#     'globaltoc_includehidden': True,
-# }
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_theme_options = {
+    'navbar_title': "Paper Journal Club",
+    'globaltoc_depth': 2,
+    'globaltoc_collapse': True, 
+    'globaltoc_includehidden': True,
+}
